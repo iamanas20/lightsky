5ad59374179a97c0df88b3c789df5fb0ca9f57d8
iamanas20 <anaslatique@gmail.com>
2022-10-27 21:44:42 +0200

    next 13 cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,58 @@
-import { demos } from '@/lib/demos';
-import Link from 'next/link';
+import '@/styles/dist.css';
+import React from 'react';
+import AddressBar from '@/ui/AddressBar';
+import GlobalNav from './GlobalNav';
 
-export default function Page() {
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="space-y-6">
-      <div className="space-y-8 text-white">
-        {demos
-          .filter((section) =>
-            section.items.some((x) => typeof x.isDisabled === 'undefined'),
-          )
-          .map((section) => {
-            return (
-              <div key={section.name} className="space-y-3">
-                <div className="text-xs font-semibold uppercase tracking-wider text-zinc-500">
-                  {section.name}
-                </div>
+    <html>
+      <head>
+        <title>Next.js Turbopack App Directory Playground</title>
+      </head>
+      <body className="overflow-y-scroll bg-zinc-900">
+        <div className="grid grid-cols-[1fr,minmax(auto,240px),min(800px,100%),1fr] gap-x-8 py-8">
+          <div className="col-start-2">
+            <GlobalNav />
+          </div>
 
-                <div className="grid grid-cols-2 gap-5">
-                  {section.items
-                    .filter((item) => !item.isDisabled)
-                    .map((item) => {
-                      return (
-                        <Link
-                          href={`/${item.slug}`}
-                          key={item.name}
-                          className="block space-y-1.5 rounded-lg border border-white/10 px-4 py-3 hover:border-white/20"
-                        >
-                          <div>{item.name}</div>
+          <div className="col-start-3 space-y-6">
+            <AddressBar />
 
-                          {item.description ? (
-                            <div className="line-clamp-3 text-sm text-zinc-400">
-                              {item.description}
-                            </div>
-                          ) : null}
-                        </Link>
-                      );
-                    })}
-                </div>
-              </div>
-            );
-          })}
-      </div>
-    </div>
+            <div className="rounded-xl border border-zinc-800 bg-black p-8">
+              {children}
+            </div>
+          </div>
+
+          <div className="col-start-3 col-end-4 mt-28 flex items-center justify-center">
+            <div className="text-sm text-zinc-600">
+              Created by the <b>Next.js</b>
+              {' team at '}
+              <a href="https://vercel.com">
+                <b>Vercel</b>
+              </a>
+              {'. '}
+              <a
+                className="underline decoration-dotted underline-offset-4"
+                href="https://github.com/vercel/next.js/examples/with-turbopack"
+              >
+                View the code
+              </a>
+              {' or '}
+              <a
+                className="underline decoration-dotted underline-offset-4"
+                href="https://vercel.com/templates/next.js"
+              >
+                deploy your own
+              </a>
+              {'.'}
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
