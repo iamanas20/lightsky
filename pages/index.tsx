1769f679e84da6832ee2f27503cabd60011f9d76
iamanas20 <anaslatique@gmail.com>
2022-11-02 21:08:02 +0100

    added the first card's style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import { Card } from '../components';
 import styles from '../styles/Home.module.scss';
 
@@ -8,7 +9,27 @@ const Home: NextPage = () => {
       <main>
         <div className={styles.topCardSection}>
           <Card className={styles.overviewCard}>
-            Hello
+            <Image src="/weather-icons/mcloudy.svg" layout="intrinsic" width="120px" height="120px"/>
+            <div className={styles.temperature}>
+              28°<span>C</span>
+            </div>
+            <div className={styles.description}>
+              <Image src="/weather-small-icons/mcloudy-icon.svg" layout="intrinsic" width="28px" height="28px"/>
+              <span>Rainy Storm Clouds</span>
+            </div>
+            <div className={styles.separator}/>
+            <div className={styles.location}>
+              <Image src="/icons/calendar.svg" layout="intrinsic" width="18px" height="18px"/>
+              <span>
+                Miami, USA.
+              </span>
+            </div>
+            <div className={styles.time}>
+              <Image src="/icons/map-pin.svg" layout="intrinsic" width="18px" height="18px"/>
+              <span>
+                { new Date().toLocaleString() }
+              </span>
+            </div>
           </Card>
           <Card className={styles.dataCard}>
             Hello
